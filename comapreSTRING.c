c174862cb0bcfef5d948ce597234a8167a01a2cd
AQUIB HASWARE <32031169+AquibHasware@users.noreply.github.com>
2017-11-17 02:01:42 +0530

    Update comapreSTRING.c

--- comapreSTRING.c
+++ comapreSTRING.c
@@ -35,3 +35,19 @@ int main()
 
     return 0;
 }
+int compare(char * str1, char * str2)
+{
+    int i = 0;
+
+    
+    while(str1[i] == str2[i])
+    {
+        if(str1[i] == '\0' && str2[i] == '\0')
+            break;
+
+        i++;
+    }
+
+    
+    return str1[i] - str2[i];
+}
